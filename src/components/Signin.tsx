f5a42e0cede9764b316ad11bb6ad2deca137e0c2
shayanzarbash <shayan.zarbash@gmail.com>
2023-01-23 17:10:05 +0330

    add new

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 const Signin = () => {
+
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const { createUser } = UserAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    try {
+      await createUser(email, password);
+    } catch (e) {
+      setError(e.massage);
+      console.log(e.massage);
+    }
+  }
+
+
+  
   return (
     <div className='max-w-[700px] max-auto m-16 p-4'>
       <div>
@@ -11,14 +33,14 @@ const Signin = () => {
           <Link className='underline' to='/signup'>sign up</Link>
         </p>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className='flex flex-col py-2'>
           <label className='py-2 font-medium text-left'>Email</label>
-          <input type='email' className='border p-3' />
+          <input type='email' onChange={(e) => setEmail(e.target.value)} className='border p-3' />
         </div>
         <div className='flex flex-col py-2'>
-        <label className='py-2 font-medium text-left'>password</label>
-          <input type='password' className='border p-3' />
+          <label className='py-2 font-medium text-left'>password</label>
+          <input type='password' onChange={(e) => setPassword(e.target.value)} className='border p-3' />
         </div>
         <button className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">sign in</button>
       </form>
